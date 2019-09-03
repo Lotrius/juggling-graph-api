527f8bc5b0cd8e508ed094cb67a6e02da3f477a3
Somesk278 <skim7420@gmail.com>
2019-09-03 00:22:04 -0400

    fixed timezone error

--- controllers/dailygraph.js
+++ controllers/dailygraph.js
@@ -2,11 +2,12 @@ const getGraphData = (req, res, db) => {
     const { newDate } = req.body;
 
     db.select('catches').from('dailydata')
-        .where(db.raw('to_char(date, \'YYYY-MM-DD\')'), 'like', newDate + '%')
+        .where(db.raw('to_char(cast((date at time zone \'America/New_York\') as date),\'YYYY-MM-DD\')'), 'like', newDate + '%')
         .then(data => {
             res.json(data);
         })
         .catch(err => res.status(400).json('Error getting data'))
+        
 }
 
 module.exports = {
