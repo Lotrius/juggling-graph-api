19e2ed9018045f5c1cfa89550aaaaf4c7388d5b8
Somesk278 <skim7420@gmail.com>
2019-09-03 00:34:15 -0400

    put app.listen back

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const app = express(); // So we can have a server going
 app.use(bodyParser.json()); // So we can get form data
 app.use(cors()); // To connect front and back end
 
-// Connect to DB
 const db = knex({
     client: 'pg',
     connection: {
@@ -25,3 +24,7 @@ app.post('/dailyupdate', (req, res) => { dailyupdate.updateData(req, res, db) })
 app.post('/dailygraph', (req, res) => { dailygraph.getGraphData(req, res, db) });
 
 app.get('/averagegraph', (req, res) => { averagegraph.getGraphData(req, res, db) });
+
+app.listen(process.env.PORT, () => {
+    console.log('working on', process.env.PORT);
+})
