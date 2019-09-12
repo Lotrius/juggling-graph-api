fb112790143af806dca44ccab6904f2d884563e5
Somesk278 <skim7420@gmail.com>
2019-09-12 09:26:41 -0400

    added signin path

---
+++ controllers/signin.js
@@ -0,0 +1,24 @@
+const handleSignIn = (req, res, db, bcrypt) => {
+    const { email, password } = req.body;
+
+    // Back end should do its own validation. Don't trust front end
+    if (!email  || !password) {
+        return res.status(400).json('Incorrect form submission');
+    }
+
+    db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return res.json('success');
+            } else {
+                res.status(400).json('Wrong credentials');
+            }
+        })
+        .catch(err => res.status(400).json('Wrong credentials'));
+}
+
+module.exports = {
+    handleSignIn: handleSignIn
+};
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
 const dailyupdate = require('./controllers/dailyupdate');
 const dailygraph = require('./controllers/dailygraph');
 const averagegraph = require('./controllers/averagegraph');
+const signin = require('./controllers/signin');
 
 const app = express(); // So we can have a server going
 
@@ -12,11 +14,23 @@ app.use(bodyParser.json()); // So we can get form data
 app.use(cors()); // To connect front and back end
 
 // Our DB
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: true
+//     }
+// });
+
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
+        host: '127.0.0.1',
+        port: '5433',
+        user: 'postgres',
+        password: '6iofgN732',
+        database: 'juggling-chart'
+
     }
 });
 
@@ -26,6 +40,8 @@ app.post('/dailygraph', (req, res) => { dailygraph.getGraphData(req, res, db) })
 
 app.post('/averagegraph', (req, res) => { averagegraph.getGraphData(req, res, db) });
 
-app.listen(process.env.PORT, () => {
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
+
+app.listen(3000 || process.env.PORT, () => {
     console.log('working on', process.env.PORT);
 })
